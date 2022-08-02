b18be6894125e1d168ab5fe8b5b41e70f0fe422d
Lukas Kryzauskas <lukosius181@gmail.com>
2022-08-02 14:17:13 +0300

    Lost all scene progress from ladder commit onwards, remade the scene and implemented the enemy movement

--- Assets/TileVania/Scripts/EnemyMovement.cs
+++ Assets/TileVania/Scripts/EnemyMovement.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidbody;
 
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-Mathf.Sign(myRigidbody.velocity.x), 1f);
     }
 }
