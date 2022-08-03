248922f19a1d9d257c1a44baa3c48d95b49c8c8e
Lukas Kryzauskas <lukosius181@gmail.com>
2022-08-03 10:31:40 +0300

    Added more levels, level loading and Game Session manager to keep track of persistant info between scenes.

---
+++ Assets/TileVania/Scripts/GameSession.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int playerLives = 3;
+    void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if(numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void ProcessPlayerDeath()
+    {
+        if(playerLives > 1)
+        {
+            RemoveLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+
+    private void RemoveLife()
+    {
+        playerLives--;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void ResetGameSession()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+}
